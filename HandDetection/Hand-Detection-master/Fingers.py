7640471a8ca148e2cfdc32043fbcaec210a86482
Sam O'Neill <37700999+Sam-ONeill@users.noreply.github.com>
2021-07-20 12:09:51 +0100

    Added Variables for All Hand Points

--- HandDetection/Hand-Detection-master/Fingers.py
+++ HandDetection/Hand-Detection-master/Fingers.py
@@ -12,6 +12,60 @@ while True:
     count = 0
     letter = ""
 
+    Pinky_Tip_H = handLandmarks[20][2]
+    Pinky_Dip_H = handLandmarks[19][2]
+    Pinky_Pip_H = handLandmarks[18][2]
+    Pinky_MCP_H = handLandmarks[17][2]
+
+    Ring_Tip_H = handLandmarks[16][2]
+    Ring_Dip_H = handLandmarks[15][2]
+    Ring_Pip_H = handLandmarks[14][2]
+    Ring_MCP_H = handLandmarks[13][2]
+
+    Middle_Tip_H = handLandmarks[12][2]
+    Middle_Dip_H = handLandmarks[11][2]
+    Middle_Pip_H = handLandmarks[10][2]
+    Middle_MCP_H = handLandmarks[9][2]
+
+    Index_Tip_H = handLandmarks[8][2]
+    Index_Dip_H = handLandmarks[7][2]
+    Index_Pip_H = handLandmarks[6][2]
+    Index_MCP_H = handLandmarks[5][2]
+
+    Thumb_Tip_H = handLandmarks[4][2]
+    Thumb_Dip_H = handLandmarks[3][2]
+    Thumb_Pip_H = handLandmarks[2][2]
+    Thumb_MCP_H = handLandmarks[1][2]
+
+    Wrist_H = handLandmarks[0][2]
+
+    Pinky_Tip_W = handLandmarks[20][1]
+    Pinky_Dip_W = handLandmarks[19][1]
+    Pinky_Pip_W = handLandmarks[18][1]
+    Pinky_MCP_W = handLandmarks[17][1]
+
+    Ring_Tip_W = handLandmarks[16][1]
+    Ring_Dip_W = handLandmarks[15][1]
+    Ring_Pip_W = handLandmarks[14][1]
+    Ring_MCP_W = handLandmarks[13][1]
+
+    Middle_Tip_W = handLandmarks[12][1]
+    Middle_Dip_W = handLandmarks[11][1]
+    Middle_Pip_W = handLandmarks[10][1]
+    Middle_MCP_W = handLandmarks[9][1]
+
+    Index_Tip_W = handLandmarks[8][1]
+    Index_Dip_W = handLandmarks[7][1]
+    Index_Pip_W = handLandmarks[6][1]
+    Index_MCP_W = handLandmarks[5][1]
+
+    Thumb_Tip_W = handLandmarks[4][1]
+    Thumb_Dip_W = handLandmarks[3][1]
+    Thumb_Pip_W = handLandmarks[2][1]
+    Thumb_MCP_W = handLandmarks[1][1]
+
+    Wrist_W = handLandmarks[0][1]
+
     if (len(handLandmarks) != 0):
         # we will get y coordinate of finger-tip and check if it lies above middle landmark of that finger
         # details: https://google.github.io/mediapipe/solutions/hands
@@ -35,3 +89,7 @@ while True:
         cv2.putText(image, letter, (45, 375), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 25)
         cv2.imshow("Volume", image)
         cv2.waitKey(1)
+
+    for i in handLandmarks:
+        for j in i:
+            print(f" i {i} j {j} ")
